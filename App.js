69ae8f4f19f8121248a25d54e569020daa2130dd
Kashyap Sureshchandra Patel <20ce086.kashyappatel@gmail.com>
2023-09-08 11:58:54 +0530

    Platform Specific Code for iOS and android

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native';
+
+import CustomButton from './components/CustomButton/CustomButton';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safeContainer}>
+      <View style={styles.container}>
+        <View style={styles.box}>
+          <Text style={styles.text}>Welcome!</Text>
+          <CustomButton title={"Press"} onPress={() => alert("Pressed")} />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safeContainer: {flex:1, backgroundColor:"plum"},
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'plum',
+    paddingTop: Platform.OS === 'ios' ? 35 : 45
   },
+  box: {
+    padding: 20,
+  },
+  text: {
+    ...Platform.select({
+      ios: {
+        color: "purple",
+        fontSize: 25,
+        fontStyle: "italic"
+      },
+      android: {
+        color: "red",
+        fontSize: 20,
+        fontStyle: "Bold"
+      }
+    }),
+    // fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center"
+  }
 });
